d9f09b4e51e12032ba198209b6478b693393b057
Mathieu Geissler <contact@mathieugeissler.fr>
2020-08-07 12:52:12 +0200

    refactoring to typscript

--- src/refactoring.ts
+++ src/refactoring.ts
@@ -1,11 +1,10 @@
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 const API = 'https://randomuser.me/api';
 
-
-async function generateRandomUser(...userNationalities) {
-  const usersCreated = [];
+async function generateRandomUser(...userNationalities: Array<Nationality>) {
+  const usersCreated: Array<RandomUser> = [];
   for (const nat of userNationalities) {
-    const criteria = new Map();
+    const criteria: Map<string, string> = new Map();
     criteria.set('nat', nat);
     try {
       const user = await getRandomUser(criteria);
@@ -17,24 +16,111 @@ async function generateRandomUser(...userNationalities) {
   console.log(usersCreated);
 }
 
-async function getRandomUser(criterias) {
+async function getRandomUser(criterias: Map<string, string>): Promise<RandomUser> {
   console.log('DEBUG -- Generate user with criterias', criterias);
+  let url = `${API}`;
   if (criterias && criterias.size) {
     // convert Map to searchParams
-    const searchParams = new URLSearchParams(criterias);
+    const criteriaObject = Array.from(criterias)
+      .reduce((obj: any, [key, val]: [string, string]) => {
+        obj[key] = val;
+        return obj;
+      }, {});
+    const searchParams = new URLSearchParams(criteriaObject);
     // create url
-    const url = `${API}/?${searchParams.toString()}`;
+    url += `/?${searchParams.toString()}`;
     console.log('DEBUG -- Generate user with url', url);
+  }
 
-    try {
-      const res = await fetch(url, {method: 'GET'});
-      const json = await res.json();
-      return json.results[0];
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
+  try {
+    const res = await fetch(url, { method: 'GET' });
+    const json = (await res.json() as RandomUserApiResponse);
+    return json.results[0];
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
 }
 
 generateRandomUser('FR', 'DE');
+
+type Nationality = string;
+
+interface RandomUserApiResponse {
+  info: RandomUserApiInfo;
+  results: Array<RandomUser>;
+}
+
+interface RandomUserApiInfo {
+  page: number
+  results: number
+  seed: string;
+  version: string;
+}
+
+interface RandomUserName {
+  title: string,
+  first: string,
+  last: string,
+}
+
+interface RandomUserLocation {
+  street: {
+    number: number;
+    name: string;
+  };
+  city: string,
+  state: string,
+  country: string,
+  postcode: number,
+  coordinates: {
+    latitude: string,
+    longitude: string,
+  },
+  timezone: {
+    offset: string,
+    description: string,
+  },
+}
+
+interface RandomUserLogin {
+  uuid: string,
+  username: string,
+  password: string,
+  salt: string,
+  md5: string,
+  sha1: string,
+  sha256: string,
+}
+
+interface RandomUserAge {
+  date: string;
+  age: number;
+}
+
+interface RandomUserId {
+  name: string,
+  value: string,
+}
+
+interface RandomUserPicture {
+  large: string;
+  medium: string;
+  thumbnail: string;
+}
+
+interface RandomUser {
+  gender: string;
+  name: RandomUserName;
+  location: RandomUserLocation;
+  email: string;
+  login: RandomUserLogin;
+  dob: RandomUserAge;
+  registered: RandomUserAge;
+  phone: string,
+  cell: string,
+  id: RandomUserId;
+  picture: RandomUserPicture;
+  nat: Nationality | Array<Nationality>,
+}
+
